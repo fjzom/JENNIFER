521617299239b9743d3d85a280954ff24f169065
fjzom <f.oviedo.m@hotmail.com>
2016-01-17 20:52:41 -0600

    Ranking
    Activity donde se mostrara la lista de los mejores apostadores ~(0 _0)~

---
+++ app/src/main/java/com/jennifer/controller/RankingsController.java
@@ -0,0 +1,61 @@
+package com.jennifer.controller;
+
+/**
+ * Created by felipe oviedo on 1/17/2016.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.jennifer.R;
+
+
+public class RankingsController extends  Activity {
+
+    ListViewAdapter adapter;
+
+    String[] titulo = new String[]{
+            "titulo1",
+            "titulo2",
+            "titulo3",
+            "titulo4",
+    };
+
+    int[] imagenes = {
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rankinglayout);
+
+        final ListView lista = (ListView) findViewById(R.id.milista);
+        adapter = new ListViewAdapter(this, titulo, imagenes);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono " + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono LARGO " + i, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+    }
+}
