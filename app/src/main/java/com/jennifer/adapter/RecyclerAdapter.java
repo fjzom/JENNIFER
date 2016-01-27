9fa534970476f42cd21d43277a3ac11400544941
fjzom <f.oviedo.m@hotmail.com>
2016-01-26 23:18:12 -0600

    update
    pop up info apuestas

--- app/src/main/java/com/jennifer/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/jennifer/adapter/RecyclerAdapter.java
@@ -2,11 +2,15 @@ package com.jennifer.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 
 import com.jennifer.R;
 import com.jennifer.controller.SecondActivity;
@@ -20,7 +24,8 @@ import java.util.List;
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
 
     private List<User> mItems;
-
+    private PopupWindow popUpWindow;
+    private LayoutInflater layoutInflater;
     public RecyclerAdapter(List<User> items) {
         mItems = items;
     }
@@ -54,6 +59,28 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
                 context.startActivity(new Intent(context, SecondActivity.class));
             }
         });
+
+        viewHolder.cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+            /*    context.startActivity(new Intent(context, SecondActivity.class));
+                Toast.makeText(context, "Your toast message.",
+                        Toast.LENGTH_SHORT).show();*/
+                layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                ViewGroup container = (ViewGroup)layoutInflater.inflate(R.layout.popup_info_apuestas,null);
+                popUpWindow = new PopupWindow(container,1000,750,true);
+                popUpWindow.showAtLocation(view, Gravity.NO_GRAVITY,500,500);
+
+                container.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popUpWindow.dismiss();
+                        return false;
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -66,12 +93,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         private final ImageView logImage;
         private final ImageView doneBtn;
         private final ImageView cancelBtn;
+        private final CardView cv;
 
         ViewHolder(View v) {
             super(v);
             logImage = (ImageView)v.findViewById(R.id.log_image);
             doneBtn = (ImageView)v.findViewById(R.id.done);
             cancelBtn = (ImageView)v.findViewById(R.id.cancel);
+            cv = (CardView)v.findViewById(R.id.cv);
         }
     }
 
